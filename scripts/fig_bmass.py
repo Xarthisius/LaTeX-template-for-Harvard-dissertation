f90ee4620d1a1730d10e2ada949818e4875f4ed6
Kacper Kowalik (Xarthisius) <xarthisius.kk@gmail.com>
2014-07-29 11:44:59 +0200

    update fig_bmass.py

--- scripts/fig_bmass.py
+++ scripts/fig_bmass.py
@@ -4,16 +4,20 @@
 import numpy as np
 import preamble
 import matplotlib.pyplot as plt
-#from matplotlib.ticker import FuncFormatter, MaxNLocator
 
+
+MEARTH = 5.97e27
 data = np.loadtxt('../data/table')
 
 fig = plt.figure(figsize=(8, 6))
 ax = fig.add_subplot(111)
-ax.semilogy(data[:, 1], data[:, 3] / 5.97e27, 'o-')
+ax.semilogy(data[:, 1], data[:, 5] / MEARTH, 'o-', label="$\\alpha=0.01$")
+ax.semilogy(data[:, 1], data[:, 3] / MEARTH, 'o-', label="$\\alpha=0.05$")
+ax.semilogy(data[:, 1], data[:, 4] / MEARTH, 'o-', label="$\\alpha=0.1$")
 ax.set_xlim((120, 300))
 ax.set_ylim((0.007, 10))
 ax.set_xlabel("Czas [rok]")
 ax.set_ylabel(u"Masa pyłu związanego grawitacyjnie $[\\textrm{M}_\oplus]$")
 fig.tight_layout()
+ax.legend(loc=4)
 fig.savefig('../figures/bmass_vs_time.pdf')
